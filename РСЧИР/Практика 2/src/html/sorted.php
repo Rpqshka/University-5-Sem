4b4639b4ac9865781f7e5926de6503d564d5c961
Alexandr Golikov <90089315+Rpqshka@users.noreply.github.com>
2022-09-21 17:37:17 +0300

    merge sort upd

--- РСЧИР/Практика 2/src/html/sorted.php
+++ РСЧИР/Практика 2/src/html/sorted.php
@@ -3,30 +3,40 @@
 $ar = htmlspecialchars($_GET["code"]);
 $a = explode(',', $ar);
 
-function quick_sort($my_array)
- {
-	$loe = $gt = array();
-	if(count($my_array) < 2)
-	{
-		return $my_array;
-	}
-	$pivot_key = key($my_array);
-	$pivot = array_shift($my_array);
-	foreach($my_array as $val)
-	{
-		if($val <= $pivot)
-		{
-			$loe[] = $val;
-		}elseif ($val > $pivot)
-		{
-			$gt[] = $val;
-		}
-	}
-	return array_merge(quick_sort($loe),array($pivot_key=>$pivot),quick_sort($gt));
+//Сортировка слиянием
+function mergeSort($arr) {
+    $count = count($arr);
+    if ($count <= 1) {
+        return $arr;
+    }
+ 
+    $left  = array_slice($arr, 0, (int)($count/2));
+    $right = array_slice($arr, (int)($count/2));
+ 
+    $left = mergeSort($left);
+    $right = mergeSort($right);
+ 
+    return merge($left, $right);
+}
+ 
+function merge(array $left, array $right) {
+    $ret = array();
+    while (count($left) > 0 && count($right) > 0) {
+        if ($left[0] < $right[0]) {
+            array_push($ret, array_shift($left));
+        } else {
+            array_push($ret, array_shift($right));
+        }
+    }
+ 
+    array_splice($ret, count($ret), 0, $left);
+    array_splice($ret, count($ret), 0, $right);
+ 
+    return $ret;
 }
  
 echo "Original Array : ".implode(',',$a). "\n";
-$my_array = quick_sort($a);
-echo 'Sorted Array : '.implode(',',$my_array);
+$arr = mergeSort($a);
+echo 'Sorted Array : '.implode(',',$arr);
 
 ?>
